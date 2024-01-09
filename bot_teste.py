bf930d1bc65972b780f35e546a8ca5dd484e99c9
Jéssica Eveline <110188236+xzxjesse@users.noreply.github.com>
2024-01-09 09:58:27 -0300

    Update bot_teste.py

--- bot_teste.py
+++ bot_teste.py
@@ -8,6 +8,7 @@ import requests
 
 #API 
 agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
 #chave: Rrhn5srkkpY2zxzdirqW9x8DOcvfDji4
 api = requests.get("https://editacodigo.com.br/index/api-whatsapp/Rrhn5srkkpY2zxzdirqW9x8DOcvfDji4", headers=agent)
 time.sleep(1)
@@ -20,6 +21,7 @@ msg_cliente = api[6].strip()
 caixa_msg2 = api[7].strip()
 caixa_pesquisa = api[8].strip()
 
+#abrir e salvar wpp
 dir_path = os.getcwd()
 chrome_options2 = Options()
 chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/pasta/sessao")
@@ -27,14 +29,17 @@ driver = webdriver.Chrome(options=chrome_options2)
 driver.get('https://web.whatsapp.com/')
 time.sleep(10)
 
+#abrir as notificações
 def bot():
     try:
-        bolinha = driver.find_element(By.CLASS_NAME, 'gz7w46tb')
-        bolinha = driver.find_elements(By.CLASS_NAME, 'gz7w46tb')
-        
+        bolinha = driver.find_element(By.CLASS_NAME, 'aumms1qt')
+        bolinha = driver.find_elements(By.CLASS_NAME, 'aumms1qt')
+                
         clica_bolinha = bolinha[-1]
         acao_bolinha = webdriver.common.action_chains.ActionChains(driver)
-        acao_bolinha.move_to_element_whith_offset(clica_bolinha, 0, -20)
+        acao_bolinha.move_to_element_with_offset(clica_bolinha, 0, -20)
+        acao_bolinha.click()
+        acao_bolinha.perform()
         acao_bolinha.click()
         acao_bolinha.perform()
 
